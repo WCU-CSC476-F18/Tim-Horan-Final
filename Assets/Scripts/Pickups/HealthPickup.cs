f4f5a0f27853965611e618abc82dbaf4bd955887
timhoran20 <timhoran20+git@gmail.com>
2018-12-02 19:11:27 -0500

    created the health pickup prefab

---
+++ Assets/Scripts/Pickups/HealthPickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(new Vector3(0, 1, 0));
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            GameObject player = collision.gameObject;
+            if(player.GetComponent<PlayerHealth>().currentHealth < 100)
+            {
+
+            }
+        }
+    }
+}

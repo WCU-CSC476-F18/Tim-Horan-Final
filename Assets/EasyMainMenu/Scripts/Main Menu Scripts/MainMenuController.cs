037ef2ea50f64b15b2e42bbb2502ea667d254a0d
timhoran20 <timhoran20+git@gmail.com>
2018-12-12 15:38:45 -0500

    added a main menu

---
+++ Assets/EasyMainMenu/Scripts/Main Menu Scripts/MainMenuController.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+    Animator anim;
+
+    public string newGameSceneName;
+    public int quickSaveSlotID;
+
+    [Header("Options Panel")]
+    public GameObject MainOptionsPanel;
+    public GameObject StartGameOptionsPanel;
+    public GameObject GamePanel;
+    public GameObject ControlsPanel;
+    public GameObject GfxPanel;
+    public GameObject LoadGamePanel;
+
+    public static bool waves = true;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+
+        //new key
+        PlayerPrefs.SetInt("quickSaveSlot", quickSaveSlotID);
+    }
+
+    #region Open Different panels
+
+    public void openOptions()
+    {
+        //enable respective panel
+        MainOptionsPanel.SetActive(true);
+        StartGameOptionsPanel.SetActive(false);
+
+        //play anim for opening main options panel
+        anim.Play("buttonTweenAnims_on");
+
+        //play click sfx
+        playClickSound();
+
+        //enable BLUR
+        //Camera.main.GetComponent<Animator>().Play("BlurOn");
+       
+    }
+
+    public void openStartGameOptions()
+    {
+        //enable respective panel
+        StartGameOptionsPanel.SetActive(true);
+
+        //play anim for opening main options panel
+        anim.Play("buttonTweenAnims_on");
+
+        //play click sfx
+        playClickSound();
+
+        //enable BLUR
+        //Camera.main.GetComponent<Animator>().Play("BlurOn");
+        
+    }
+
+    public void openOptions_Game()
+    {
+        //enable respective panel
+        GamePanel.SetActive(true);
+        ControlsPanel.SetActive(false);
+        GfxPanel.SetActive(false);
+        LoadGamePanel.SetActive(false);
+
+        //play anim for opening game options panel
+        anim.Play("OptTweenAnim_on");
+
+        //play click sfx
+        playClickSound();
+
+    }
+    public void openOptions_Controls()
+    {
+        //enable respective panel
+        GamePanel.SetActive(false);
+        ControlsPanel.SetActive(true);
+        GfxPanel.SetActive(false);
+        LoadGamePanel.SetActive(false);
+
+        //play anim for opening game options panel
+        anim.Play("OptTweenAnim_on");
+
+        //play click sfx
+        playClickSound();
+
+    }
+    public void openOptions_Gfx()
+    {
+        //enable respective panel
+        GamePanel.SetActive(false);
+        ControlsPanel.SetActive(false);
+        GfxPanel.SetActive(true);
+        LoadGamePanel.SetActive(false);
+
+        //play anim for opening game options panel
+        anim.Play("OptTweenAnim_on");
+
+        //play click sfx
+        playClickSound();
+
+    }
+
+    public void openContinue_Load()
+    {
+        waves = false;
+        SceneManager.LoadScene("Level 01");
+
+    }
+
+    public void newGame()
+    {
+        waves = true;
+        SceneManager.LoadScene("Level 01");
+    }
+    #endregion
+
+    #region Back Buttons
+
+    public void back_options()
+    {
+        //simply play anim for CLOSING main options panel
+        anim.Play("buttonTweenAnims_off");
+
+        //disable BLUR
+       // Camera.main.GetComponent<Animator>().Play("BlurOff");
+
+        //play click sfx
+        playClickSound();
+    }
+
+    public void back_options_panels()
+    {
+        //simply play anim for CLOSING main options panel
+        anim.Play("OptTweenAnim_off");
+        
+        //play click sfx
+        playClickSound();
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    #endregion
+
+    #region Sounds
+    public void playHoverClip()
+    {
+       
+    }
+
+    void playClickSound() {
+
+    }
+
+
+    #endregion
+}
